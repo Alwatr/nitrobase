a6d63b34241e26e275a3e00f6306e3545bf5a4f5
Ali Mihandoost <alimihandoost@gmail.com>
2020-12-28 20:36:59 +0330

    feat(util): add utcTimestamp

--- src/util.ts
+++ src/util.ts
@@ -3,12 +3,12 @@ import { resolve, dirname } from 'path';
 import { debug } from 'debug';
 export const log = debug('1db');
 
-export async function readJsonFile <T extends Record<string | number, unknown>>(path: string): Promise<T | null> {
+export async function readJsonFile <T extends Record<string | number, unknown>>(path: string): Promise<T> {
   log(`readJsonFile ${path}`);
 
   path = resolve(path);
   if (!existsSync(path)) {
-    return null;
+    throw new Error('file_not_found');
   }
 
   const fileContent = await fs.readFile(path, {encoding: 'utf-8'});
@@ -29,3 +29,8 @@ export async function writeJsonFile (path: string, data: unknown): Promise<void>
   await fs.writeFile(path, json, {encoding: 'utf-8'});
   log(`writeJsonFile: ${json.length} characters saved`);
 };
+
+export function utcTimestamp (): number {
+  const now = new Date();
+  return now.getTime() + now.getTimezoneOffset() * 60000;
+}
