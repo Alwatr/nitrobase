44c684c44effbc89b39e50c847e9434c229e8def
Ali Mihandoost <alimihandoost@gmail.com>
2021-01-02 19:34:40 +0330

    fix(1db): review and fix bugs

--- src/1db.ts
+++ src/1db.ts
@@ -12,31 +12,31 @@ export interface DocumentRecord extends Record<string, unknown> {
 }
 
 export class OneDB {
-  private path: string;
-  private storage: Promise<DocumentStorage>;
-  private indexList?: string[];
+  private _path: string;
+  private _indexList?: string[];
+  _storage: Promise<DocumentStorage>;
 
   /**
    * Open JSON file
    */
   constructor (storagePath: string) {
     log(`open ${storagePath}`);
-    this.path = storagePath;
-    this.storage = readJsonFile<DocumentStorage>(this.path!);
-    this.storage.catch(err => { throw err });
+    this._path = storagePath;
+    this._storage = readJsonFile<DocumentStorage>(this._path);
+    this._storage.catch(err => { throw err });
   }
 
   protected async _updateIndexList(id?: string) {
-    if (id != undefined && this.indexList == undefined) { return; } // no index created yet and maybe not need at all
+    if (id != undefined && this._indexList == undefined) { return; } // no index created yet and maybe not need at all
     
-    const storage = await this.storage;
+    const storage = await this._storage;
 
-    if (this.indexList == undefined) {
-      this.indexList = Object.keys(storage)
+    if (this._indexList == undefined) {
+      this._indexList = Object.keys(storage)
     }
 
-    if (id != undefined && !(id in this.indexList)) {
-      this.indexList.push(id);
+    if (id != undefined && !(id in this._indexList)) {
+      this._indexList.push(id);
     }
   }
 
@@ -47,10 +47,10 @@ export class OneDB {
     log(`set ${id}`);
     if (id === '_latest') { throw new Error('forbidden_key'); }
 
-    const storage = await this.storage;
+    const storage = await this._storage;
 
     let oldData = storage[id];
-    if (typeof oldData !== 'object') {
+    if (oldData !== undefined && !(typeof oldData === 'object' && '_id' in oldData)) {
       oldData = undefined; // invalid data!
     }
 
@@ -58,7 +58,7 @@ export class OneDB {
     data._modified = oldData?._modified ?? utcTimestamp();
     data._created = oldData?._created ?? data._modified;
 
-    if (!replace) {
+    if (data !== oldData && !replace) {
       data = {
         ...oldData,
         ...data,
@@ -66,6 +66,7 @@ export class OneDB {
     }
 
     storage[id] = data;
+    storage._latest = id;
     void this._updateIndexList(id);
 
     this.saveRequest();
@@ -77,8 +78,8 @@ export class OneDB {
    */
   async get<T extends DocumentRecord>(id: string): Promise<T | undefined> {
     log(`get ${id}`);
-    if (id == null) return undefined;
-    const storage = await this.storage;
+    if (!(id != null && typeof id === 'string' && id.length > 0)) { return; }
+    const storage = await this._storage;
     const data = storage[id];
     if (typeof data === 'string') {
       return this.get(data);
@@ -89,23 +90,15 @@ export class OneDB {
   }
 
   /**
-   * Get all items
-   */
-  async _getStorage(): Promise<DocumentStorage> {
-    log('_getStorage');
-    return this.storage;
-  }
-
-  /**
    * Find single item
    */
   async find(predicate: (documentRecord: DocumentRecord) => boolean): Promise<DocumentRecord | undefined> {
     log('find');
     await this._updateIndexList();
-    const storage = await this.storage;
-    for (const id of this.indexList!) {
+    const storage = await this._storage;
+    for (const id of this._indexList!) {
       const documentRecord = storage[id];
-      if (documentRecord != undefined && typeof documentRecord !== 'string' && predicate(documentRecord)) {
+      if (documentRecord != null && typeof documentRecord === 'object' && predicate(documentRecord)) {
         return documentRecord;
       }
     }
@@ -118,11 +111,11 @@ export class OneDB {
   async findAll(predicate: (documentRecord: DocumentRecord) => boolean): Promise<Array<DocumentRecord>> {
     log('findAll');
     await this._updateIndexList();
-    const storage = await this.storage;
+    const storage = await this._storage;
     const result: Array<DocumentRecord> = [];
-    for (const id of this.indexList!) {
+    for (const id of this._indexList!) {
       const documentRecord = storage[id];
-      if (documentRecord != undefined && typeof documentRecord !== 'string' && predicate(documentRecord)) {
+      if (documentRecord != null && typeof documentRecord === 'object' && predicate(documentRecord)) {
         result.push(documentRecord);
       }
     }
@@ -134,15 +127,15 @@ export class OneDB {
    */
   async delete(id: string): Promise<void> {
     log('delete', id);
-    const storage = await this.storage;
+    const storage = await this._storage;
     delete storage[id];
-    delete this.indexList; // need to re-index
+    delete this._indexList; // need to re-index
     this.saveRequest();
   }
 
   saveRequest = debounce((): void => {
     log('Save db');
-    writeJsonFile(this.path, this.storage);
+    this._storage.then(storage => writeJsonFile(this._path, storage))
   }, 100, {
     leading: false,
     trailing: true,
