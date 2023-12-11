9801fbeb81bdfc6ccddd015e2addcd8a08eff132
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-12-12 01:37:11 +0330

    feat(engine5): Add global variable for package version in logger.ts

--- packages/engine5/src/lib/logger.ts
+++ packages/engine5/src/lib/logger.ts
@@ -1,5 +1,10 @@
 import {createLogger, definePackage} from '@alwatr/logger';
 
-definePackage('store-engine', '5.x');
+declare global {
+  // eslint-disable-next-line no-var
+  var __package_version: string;
+}
+
+definePackage('store-engine', __package_version);
 
 export const logger = createLogger('store-engine');
