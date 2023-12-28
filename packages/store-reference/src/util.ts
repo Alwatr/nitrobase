3343136a481a0bfa15c6c8f8ec64ce31387a8887
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-12-28 15:01:37 +0330

    feat(store-reference): Add utility functions for converting StoreFileId and StoreFileStat to string IDs and paths

---
+++ packages/store-reference/src/util.ts
@@ -0,0 +1,34 @@
+import {flatString} from '@alwatr/flat-string';
+import {
+  type StoreFileId,
+  type StoreFileStat,
+} from '@alwatr/store-types';
+
+/**
+ * Convert StoreFileId to a string ID.
+ *
+ * @param storeId - The StoreFileId.
+ * @returns The store ID as a string.
+ */
+export function getStoreId(storeId: StoreFileId): string {
+  let id = storeId.region + '/' + storeId.name;
+  if (storeId.ownerId !== undefined) {
+    id += '/' + storeId.ownerId;
+  }
+  return flatString(id);
+}
+
+/**
+ * Returns the store path based on the provided storeStat object.
+ *
+ * @param storeStat The store file stat object.
+ * @returns The store path.
+ */
+export function getStorePath(storeStat: StoreFileStat): string {
+  let path: string = storeStat.region;
+  if (storeStat.ownerId !== undefined) {
+    path += '/' + storeStat.ownerId.slice(0, 3) + '/' + storeStat.ownerId;
+  }
+  path += `/${storeStat.name}.${storeStat.type}.${storeStat.extension}`;
+  return flatString(path);
+}
